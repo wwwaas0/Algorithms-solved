8e27dde03e098fc80fad26dd04dfde63f3ca6104
wwwaas0 <wwwaas0@ajou.ac.kr>
2023-08-10 01:37:15 +0900

    no.1427

---
+++ baekjun/silver/silver_5/sort_inside-1427/src/Main.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split("");
+
+        Arrays.sort(input, Collections.reverseOrder());
+        for(String s : input){
+            System.out.print(s);
+        }
+    }
+}
